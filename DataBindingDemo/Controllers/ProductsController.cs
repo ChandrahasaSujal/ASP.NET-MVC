169592a93814550c24dbbae657380b1f5e7c65b8
chanducodemonk <chanducodemonk@gmail.com>
2018-11-09 11:55:50 +0530

    My first MVC App. with DataBinding in Model & View. so excited to learning this technology.

---
+++ DataBindingDemo/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataBindingDemo.Models;
+
+namespace DataBindingDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsData db = new ProductsData();
+
+        public ViewResult Index()
+        {
+           List<Product> products= db.ProductList.ToList();
+            return View(products);
+        }
+
+        public ViewResult Details(int id)
+        {
+           Product product= db.ProductList.Single(x => x.ProductId == id);
+            return View(product);
+        }
+    }
+}
---
+++ DataBindingDemo/Models/ProductsData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataBindingDemo.Models
+{
+    public class ProductsData
+    {
+        List<Product> Products = new List<Product>()
+        {
+            new Product{ProductId=1,Name="Samsung TV",Price=3400.53,Photo="~/Images/tv.jpg"},
+            new Product{ProductId=2,Name="Nike Shoe",Price=4200.41,Photo="~/Images/shoe.png"}
+        };
+
+        public IEnumerable<Product>
+         ProductList
+        {
+            get
+            {
+                return Products;
+            }
+        }
+    }
+}
